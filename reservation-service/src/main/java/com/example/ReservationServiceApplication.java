0fd512ac58417bea1279304081eb27d1453950de
Carl Klagba <c.klagba@paris-turf.com>
2016-09-09 17:50:00 +0200

    service

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -3,9 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.exemple")
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
