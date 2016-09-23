9eaff570d7f15f84083ef44474f01077851da461
CarlKlagba <klagba@gmail.com>
2016-09-23 17:59:21 +0200

    okok

--- zuul-server/src/main/java/com/example/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/example/ZuulServerApplication.java
@@ -1,14 +1,11 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableAutoConfiguration
-@EnableEurekaClient
 @EnableZuulProxy
+@SpringBootApplication
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
